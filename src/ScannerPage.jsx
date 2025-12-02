6ff2f3db5cad0ef2564986015e06cf077edbf4fb
togaevtemirlan7-spec <togaevtemirlan7@gmail.com>
2025-12-02 15:41:47 +0500

    Update ScannerPage.jsx

--- src/ScannerPage.jsx
+++ src/ScannerPage.jsx
@@ -1,32 +1,26 @@
-import { useEffect, useRef } from "react";
 import QrScanner from "qr-scanner";
+import { useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ScannerPage() {
   const videoRef = useRef(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const scanner = new QrScanner(
-      videoRef.current,
-      (result) => {
-        window.location.href = result.data;
-      },
-      {
-        highlightScanRegion: true,
-        highlightCodeOutline: true,
-      }
-    );
+    const scanner = new QrScanner(videoRef.current, (result) => {
+      navigate(result.data);
+      scanner.stop();
+    });
 
     scanner.start();
+
     return () => scanner.stop();
   }, []);
 
   return (
     <div className="container">
       <h1>Сканер QR</h1>
-      <video
-        ref={videoRef}
-        style={{ width: "100%", borderRadius: 10, marginTop: 20 }}
-      ></video>
+      <video ref={videoRef} style={{ width: "100%" }}></video>
     </div>
   );
 }
