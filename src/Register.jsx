f67a3e97dc6117c27e6cadc8114ae960b2184603
togaevtemirlan7-spec <togaevtemirlan7@gmail.com>
2025-12-02 15:57:49 +0500

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "./supabaseClient";
 import { useNavigate } from "react-router-dom";
 
 export default function Register() {
@@ -130,3 +130,4 @@ export default function Register() {
     </div>
   );
 }
+
